bce06d735ed08e2cbc5be6bf4e273d7f0a35b463
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-03-30 12:17:15 +0300

    renderbook function

--- public/index.js
+++ public/index.js
@@ -1,11 +1,17 @@
-function getBooks(callback) {
-    // make request to our server (/get-books)
-}
+    function getBooks(callback) {
+        // make request to our server (/get-books)
+    }
 
-function renderBooks(error, booksArray) {
-    // should receive an array of book objects
-    // should render them to the page
-}
+    function renderBooks(error, booksArray) {
+        // should receive an array of book objects
+        if (error) {
+            var errorParagraph = document.createElement('p');
+            errorParagraph.innerHTML = 'Error!!';
+        }
+        
 
-// get books and render to page
-getBooks(renderList);
+        // should render them to the page
+    }
+
+    // get books and render to page
+    getBooks(renderList);
