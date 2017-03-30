af635803b668d6ba534947dd40b4d30df108e6de
hiba <hibaakel@github.com>
2017-03-30 15:07:50 +0300

    make the getbook-handler in handler page

--- src/handlers.js
+++ src/handlers.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var path = require('path');
+const connPool = require('./db_connection.js');
 
 var headers = {
     'html' : {'content-type' : 'text/html'},
     'css' : {'content-type' : 'text/css'},
-    'js' : {'content-type' : 'text/javascript'}
+    'js' : {'content-type' : 'application/javascript'},
+    'json':{'content-type' : 'application/json'}
 }
 
 var handlers = {};
@@ -22,7 +24,18 @@ handlers.addReservation = function(req, res) {
 }
 
 handlers.getBooks = function(req, res) {
-
+    //connect to the db
+    // make query to get all the books
+    connPool.query("SELECT * FROM books", function(err,results) {
+        if (err) {
+            res.writehead(500, headers.plain);
+            res.end('err with database query');
+        }
+        else {
+            res.writehead(200,headers.json);
+            res.end(JSON.stringify(results.rows));
+        }
+    });
 }
 
 handlers.notFound = function(req, res) {
