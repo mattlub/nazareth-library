778b6141c24c61199125cd2f503a03ff890a1f03
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-03-30 11:57:18 +0300

    make a getBooks function

--- public/index.js
+++ public/index.js
@@ -1,11 +1,27 @@
-function getBooks(callback) {
-    // make request to our server (/get-books)
-}
+var hibaReads = (function() {
 
-function renderBooks(error, booksArray) {
-    // should receive an array of book objects
-    // should render them to the page
-}
+    function getBooks(callback) {
+        // make request to our server (/get-books)
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readeState === 4 && xhr.status === 200){
+                callback(null, JSON.parse(xhr.responseText));
+            }
+            else if (xhr.status === 500){
+                var errorMessage = xhr.responseText;
+                callback(errorMessage, null);
+            }
+        }
+        xhr.open('GET', '/get-books');
+        xhr.send();
+    }
 
-// get books and render to page
-getBooks(renderList);
+    function renderBooks(error, booksArray) {
+        // should receive an array of book objects
+        // should render them to the page
+    }
+
+    // get books and render to page
+    getBooks(renderList);
+
+})();
