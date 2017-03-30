9234b980183a4fcceee1328fcdc38358a1db5ec5
Matt Lubel <matt_lubel@hotmail.com>
2017-03-30 12:35:20 +0300

    fix typo in db setup, remove date_created column from insert

--- database/db_build.sql
+++ database/db_build.sql
@@ -20,11 +20,11 @@ CREATE TABLE reservations (
     date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO books (title, author, owner, summary, date_created) VALUES
+INSERT INTO books (title, author, owner, summary) VALUES
 ('Book Title 1', 'Author 1', 'Mohamed', 'A short book about nothing much'),
 ('Book Title 2', 'Author 2', 'Matt', 'A short book about nothing much');
 
-INSERT INTO reservations (book_id, name, from_date, to_date, date_created) VALUES
+INSERT INTO reservations (book_id, name, from_date, to_date) VALUES
 (1, 'Hiba', '2017-04-01', '2017-04-08'),
 (1, 'Jack', '2017-05-01', '2017-05-08');
 
