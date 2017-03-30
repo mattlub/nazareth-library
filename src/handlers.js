9207046b6571c9808f3bde0b7f8090ab6d257f0c
JWLD <jackwldavies@icloud.com>
2017-03-30 15:52:15 +0300

    completed public handler in handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -10,7 +10,24 @@ var headers = {
 var handlers = {};
 
 handlers.public = function(req, res) {
+    var fileType = req.url.split('.')[1];
+    res.writeHead(200, headers[fileType] || headers['html']);
 
+    var fileName = '';
+    if (req.url === '/') {
+        fileName = 'list.html';
+    } else  {
+        fileName = req.url.slice(1);
+    }
+
+    var filePath = path.join(__dirname, '..', 'public', fileName);
+    fs.readFile(filePath, function(error, file) {
+        if (error) {
+            res.writeHead(500, headers.plain);
+            res.end('Something went wrong when reading this file: ', fileName);
+        }
+        res.end(file);
+    });
 }
 
 handlers.addBook = function(req, res) {
