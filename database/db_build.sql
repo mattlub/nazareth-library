0f27f4b1055012074bfeca0cc0886131a689ece4
Matt Lubel <matt_lubel@hotmail.com>
2017-03-30 12:31:43 +0300

    add default timestamp as date_created column in books and reservations tables

--- database/db_build.sql
+++ database/db_build.sql
@@ -8,7 +8,7 @@ CREATE TABLE books (
     author VARCHAR(100) NOT NULL,
     owner VARCHAR(30) NOT NULL,
     summary TEXT NOT NULL,
-    date_created TIMESTAMP
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE reservations (
@@ -17,15 +17,15 @@ CREATE TABLE reservations (
     name VARCHAR(30) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    date_created TIMESTAMP
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO books (title, author, owner, summary, date_created) VALUES
-('Book Title 1', 'Author 1', 'Mohamed', 'A short book about nothing much', '2017-03-30'),
-('Book Title 2', 'Author 2', 'Matt', 'A short book about nothing much', '2017-03-30');
+('Book Title 1', 'Author 1', 'Mohamed', 'A short book about nothing much'),
+('Book Title 2', 'Author 2', 'Matt', 'A short book about nothing much');
 
 INSERT INTO reservations (book_id, name, from_date, to_date, date_created) VALUES
-(1, 'Hiba', '2017-04-01', '2017-04-08', '2017-03-30'),
-(1, 'Jack', '2017-05-01', '2017-05-08', '2017-03-30');
+(1, 'Hiba', '2017-04-01', '2017-04-08'),
+(1, 'Jack', '2017-05-01', '2017-05-08');
 
 COMMIT;
