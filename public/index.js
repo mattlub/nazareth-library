dac95dfbd6de31051cfb5558a3e93fa7fcea2d69
Matt Lubel <matt_lubel@hotmail.com>
2017-04-01 15:39:41 +0300

    move render function out of index.js

--- public/index.js
+++ public/index.js
@@ -16,46 +16,6 @@ var hibaReads = (function() {
         xhr.send();
     }
 
-    // should receive an array of book objects
-    function renderBooks(error, booksArray) {
-        var booksSection = document.getElementById('books-section');
-
-        if (error) {
-            booksSection.innerHTML = ('Error!! ' + error);
-            booksSection.style.color = 'red';
-            return;
-        }
-
-        var tableElement = document.createElement('table');
-        tableElement.id = 'books-table';
-
-        // Create table headers
-        var headersRowElement = document.createElement('tr');
-        var headers = ['Title', 'Author', 'Owner', 'Summary', 'Date_Created'];
-
-        headers.forEach(function(header) {
-            var thElement = document.createElement('th');
-            thElement.innerHTML = header;
-            headersRowElement.appendChild(thElement);
-        });
-
-        tableElement.appendChild(headersRowElement);
-
-        booksArray.forEach(function(book) {
-            var trElement = document.createElement('tr');
-
-            headers.forEach(function(header) {
-                var tdElement = document.createElement('td');
-                tdElement.innerHTML = book[header.toLowerCase()];
-                trElement.appendChild(tdElement);
-            });
-            tableElement.appendChild(trElement);
-        });
-
-        // should render them to the page
-        booksSection.appendChild(tableElement);
-    }
-
-    getBooks(renderBooks);
+    getBooks(Render.renderBooksList);
 
 })();
